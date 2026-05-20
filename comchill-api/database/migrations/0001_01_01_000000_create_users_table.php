aed2ecb5f8ed95d433831bba5fa5e5afab1d3587
Hermas Francisco <franciscohermas@gmail.com>
2026-05-20 12:36:19 +0100

    feat(migration): update users table schema with additional fields and rename 'name' to 'full_name'

--- comchill-api/database/migrations/0001_01_01_000000_create_users_table.php
+++ comchill-api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('phone_number')->nullable();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('bio')->nullable();
+            $table->string('profile_photo')->nullable();
+            $table->string('password')->nullable(); // Nullable for OAuth login
+            $table->string('provider')->nullable();
+            $table->string('provider_id')->nullable();
             $table->timestamps();
         });
 
