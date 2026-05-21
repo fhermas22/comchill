4fbe0daaa2783e4f1efc86d2c8186b6fbb118d5c
Hermas Francisco <franciscohermas@gmail.com>
2026-05-21 17:13:20 +0100

    chore: delete comchill-app

--- comchill-app/lib/repositories/api_repository/auth_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:clone_whatsapp_base_code/services/auth_service.dart';
-
-class AuthRepository {
-
-  Future<Map<String, dynamic>> login(String email, String password) async {
-    return await AuthService.login(email, password);
-  }
-
-}
