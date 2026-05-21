ab84acd66de0a97792c7a96a985bd78a9dbb947c
Jr6999 <houankanjeremie@gmail.com>
2026-05-21 17:29:16 +0200

    feat(frontend): ajouter le logo et améliorer l'interface de l'écran de splash

--- comchill_app/lib/screens/splash/splash_screen.dart
+++ comchill_app/lib/screens/splash/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:comchill_app/screens/splash/controller/controller.dart'; 
+import 'package:comchill_app/utils/colors.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,14 +19,59 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: const Center(
-          child: Text(
-            'ComChill',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('assets/images/logo/comchill_logo.png'),
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(height: 20),
+                Text(
+                  'ComChill',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                  color: primaryTextColor,
+                ) ,
+              ),
+              SizedBox(height: 10),
+                          Row(
+              mainAxisAlignment: MainAxisAlignment.center, 
+              children: [
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: const BoxDecoration(
+                    color: primaryColor, 
+                    shape: BoxShape.circle,   
+                  ),
+                ),
+                const SizedBox(width: 8), 
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFE47E3A),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                const SizedBox(width: 8), 
+                Container(
+                  width: 12,
+                  height: 12,
+                  decoration: const BoxDecoration(
+                    color: primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ],
+            )
+
+
+            ]
           ),
         ),
       ),
