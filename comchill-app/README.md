a5c318b21c6a031ae2e0a3d09ae27a18526e423d
Hermas Francisco <franciscohermas@gmail.com>
2026-05-21 17:11:43 +0100

    Refactor Windows runner: remove unnecessary files and implement new structure
    - Deleted Runner.rc, flutter_window.cpp, flutter_window.h, main.cpp, resource.h, app_icon.ico, runner.exe.manifest.xml, utils.cpp, utils.h, win32_window.cpp, win32_window.h.
    - Added .gitignore to exclude build artifacts and IDE files.
    - Created GeneratedPluginRegistrant.java for Android plugin registration.
    - Set up local.properties for Android SDK and Flutter SDK paths.
    - Generated Flutter configuration files for iOS and macOS.
    - Implemented new structure for plugin registration in iOS.

--- comchill-app/README.md
+++
@@ -1,3 +0,0 @@
-# ComChill Mobile & Web App
-
-...
--- comchill-app/android/app/src/main/kotlin/com/example/clone_whatsapp_base_code/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.clone_whatsapp_base_code
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
