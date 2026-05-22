c8857dca4c4dbe47dd4e3171f7ef5d6a9ca937dc
Hermas Francisco <franciscohermas@gmail.com>
2026-05-22 10:02:27 +0100

    feat(resource): create FileResource for file conversion to array

---
+++ comchill-api/app/Http/Resources/FileResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'file_path' => $this->file_path,
+            'file_type' => $this->file_type,
+            'file_size' => $this->file_size,
+        ];
+    }
+}
